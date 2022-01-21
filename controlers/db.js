889f616ea213a31154f2b941a1eb7e8129009171
Anton <anton.v.shiryaev@gmail.com>
2022-01-21 15:46:03 +0300

    fixing db

--- controlers/db.js
+++ controlers/db.js
@@ -2,11 +2,8 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : '4608',
-      database : 'vehiclerecdb'
+      connectionString : process.env.DATABASE_URL,
+      ssl:true
     }
   });
 
